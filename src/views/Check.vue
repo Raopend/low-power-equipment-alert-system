2e1219b11097ca08bd343de43dc5c71b5b2c8b68
Peng Rao <raopend@gmail.com>
2024-05-17 00:18:25 +0800

    修复常量 bug

--- src/views/Check.vue
+++ src/views/Check.vue
@@ -1,16 +1,22 @@
 <script setup>
-import {use} from "echarts/core";
-import {LineChart} from "echarts/charts";
-import {GridComponent, LegendComponent, PolarComponent, TitleComponent, TooltipComponent} from "echarts/components";
+import { use } from "echarts/core";
+import { LineChart } from "echarts/charts";
+import {
+  GridComponent,
+  LegendComponent,
+  PolarComponent,
+  TitleComponent,
+  TooltipComponent,
+} from "echarts/components";
 import SectionTitleLineWithButton from "@/components/Base/SectionTitleLineWithButton.vue";
 import BaseButton from "@/components/Base/BaseButton.vue";
 import CardBox from "@/components/Card/CardBox.vue";
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
 import SectionMain from "@/components/Base/SectionMain.vue";
-import {mdiFlashTriangleOutline, mdiSineWave} from "@mdi/js";
+import { mdiFlashTriangleOutline, mdiSineWave } from "@mdi/js";
 import VChart from "vue-echarts";
-import {CanvasRenderer} from "echarts/renderers";
-import {onMounted, ref} from "vue";
+import { CanvasRenderer } from "echarts/renderers";
+import { onMounted, ref } from "vue";
 import axiosInstance from "@/api/axiosInstance.js";
 
 use([
@@ -20,7 +26,7 @@ use([
   LegendComponent,
   TooltipComponent,
   GridComponent,
-  CanvasRenderer
+  CanvasRenderer,
 ]);
 
 let mean = [];
@@ -39,32 +45,32 @@ let maxPoints = 100;
 
 const option = ref({
   tooltip: {
-    trigger: 'axis'
+    trigger: "axis",
   },
   xAxis: {
-    type: 'category',
+    type: "category",
     boundaryGap: false,
-    data: XData
+    data: XData,
   },
   yAxis: {
-    type: 'value',
-    min: 12,  // 设置Y轴的最小值
-    max: 24,  // 设置Y轴的最大值
+    type: "value",
+    min: 12, // 设置Y轴的最小值
+    max: 24, // 设置Y轴的最大值
   },
-  series: Array.from({length: 6}, (_, i) => ({
+  series: Array.from({ length: 6 }, (_, i) => ({
     name: `温度特征${i + 1}`,
-    type: 'line',
+    type: "line",
     smooth: true,
     showSymbol: false, // 隐藏数据点
     lineStyle: {
       width: 3,
-      type: i % 2 === 0 ? 'solid' : 'dashed', // 交替使用实线和虚线
+      type: i % 2 === 0 ? "solid" : "dashed", // 交替使用实线和虚线
     },
     emphasis: {
-      focus: 'series'
+      focus: "series",
     },
-    data: seriesData[i]
-  }))
+    data: seriesData[i],
+  })),
 });
 
 const option1 = ref({
@@ -81,37 +87,37 @@ const option1 = ref({
   },
   series: [
     {
-      name: 'Distance',
-      type: 'line',
+      name: "Distance",
+      type: "line",
       data: YData1,
       lineStyle: {
-        color: 'blue',
+        color: "blue",
         width: 3,
       },
       showSymbol: false, // 隐藏数据点
       smooth: true, // 线条平滑
     },
     {
-      name: 'Mean',
-      type: 'line',
+      name: "Mean",
+      type: "line",
       data: Array(XData1.length).fill(mean),
       showSymbol: false, // 隐藏数据点
-      lineStyle: {color: 'green', type: 'dashed'}
+      lineStyle: { color: "green", type: "dashed" },
     },
     {
-      name: 'Upper Limit',
-      type: 'line',
+      name: "Upper Limit",
+      type: "line",
       showSymbol: false, // 隐藏数据点
       data: Array(XData1.length).fill(mean + 3 * std),
-      lineStyle: {color: 'red', type: 'dashed'}
+      lineStyle: { color: "red", type: "dashed" },
     },
     {
-      name: 'Lower Limit',
-      type: 'line',
+      name: "Lower Limit",
+      type: "line",
       showSymbol: false, // 隐藏数据点
       data: Array(XData1.length).fill(mean - 3 * std),
-      lineStyle: {color: 'red', type: 'dashed'}
-    }
+      lineStyle: { color: "red", type: "dashed" },
+    },
   ],
 });
 
@@ -125,14 +131,14 @@ function addData() {
     // 检查是否需要移除旧的数据点
     if (XData.length > maxPoints) {
       XData.shift();
-      seriesData.forEach(arr => arr.shift());
+      seriesData.forEach((arr) => arr.shift());
     }
 
     // 更新图表
     option.value.xAxis.data = XData;
     option.value.series = seriesData.map((data, i) => ({
       ...option.value.series[i],
-      data
+      data,
     }));
 
     xdataIndex++;
@@ -143,7 +149,7 @@ function addData() {
 function addData1() {
   if (ydataIndex1 < distances.length) {
     XData1.push(xdataIndex1.toString()); // 使用数据点的索引作为X轴数据
-    YData1.push(distances[ydataIndex]); // 添加新的Y轴数据点
+    YData1.push(distances[ydataIndex1]); // 添加新的Y轴数据点
 
     // 检查是否需要移除旧的数据点
     if (XData1.length > maxPoints) {
@@ -175,29 +181,26 @@ onMounted(() => {
   setInterval(addData, 1000);
   setInterval(addData1, 1000);
 });
-
 </script>
 
 <template>
   <default-layout>
     <section-main>
       <section-title-line-with-button :icon="mdiSineWave" title="三相电流">
-        <base-button color="whiteDark"/>
+        <base-button color="whiteDark" />
       </section-title-line-with-button>
       <CardBox class="mb-6 mt-4">
-        <div>
-
-        </div>
+        <div></div>
       </CardBox>
 
       <section-title-line-with-button
-          :icon="mdiFlashTriangleOutline"
-          title="三相电压"
+        :icon="mdiFlashTriangleOutline"
+        title="三相电压"
       >
-        <base-button color="whiteDark"/>
+        <base-button color="whiteDark" />
       </section-title-line-with-button>
       <CardBox class="mb-6 mt-4">
-        <v-chart :option="option1" autoresize style="height: 400px"/>
+        <v-chart :option="option1" autoresize style="height: 400px" />
       </CardBox>
     </section-main>
   </default-layout>
